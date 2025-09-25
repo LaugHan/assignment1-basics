b476414f816f8e386a4f52d553ebbb1e9c68f219
LaugHan <2410764216@qq.com>
2025-09-25 12:03:42 +0800

    测试了tokenizer的encode，暂时好用

--- cs336_basics/tokenizer.py
+++ cs336_basics/tokenizer.py
@@ -10,9 +10,10 @@ class Tokenizer:
         self.special_tokens = special_tokens
 
         # 处理没有遇到过的special_token，直接加到末尾
-        for special_token in special_tokens:
-            if special_token not in vocab.values():
-                vocab[len(vocab)] = special_token.encode('utf-8')
+        if special_tokens is not None:
+            for special_token in special_tokens:
+                if special_token not in vocab.values():
+                    vocab[len(vocab)] = special_token.encode('utf-8')
 
         # 创建bytes2int的反向字典，encoding时需要从bytes直接到ID
         vocab_inverse = dict()
@@ -20,12 +21,14 @@ class Tokenizer:
             vocab_inverse[token] = ID
         self.vocab_inverse = vocab_inverse
     
+    # 加了这个修饰器后，就可以直接用这个办法初始化Tokenizer
+    @classmethod
     def from_files(cls, vocab_filepath:str, merges_filepath:str, special_tokens = None):
         with open(vocab_filepath, 'rb') as f:
             vocab = pickle.load(f)
         with open(merges_filepath, 'rb') as f:
             merges = pickle.load(f)
-        cls(vocab, merges, special_tokens)
+        return cls(vocab, merges, special_tokens)
 
 
     def index_bytes_pair(self, bytes_2_ranks:dict[tuple[bytes], int], bytes_list:list[bytes]):
@@ -48,38 +51,64 @@ class Tokenizer:
                 encoded_list.append(self.vocab_inverse[pretoken.encode('utf-8')])
                 continue
 
+
             pretoken_bytes = pretoken.encode('utf-8')
-            bytes_list = []
-            for num in list(pretoken_bytes):
-                bytes_list.append(bytes(num))
+            if len(pretoken_bytes) == 1:
+                encoded_list.append(self.vocab_inverse[bytes([pretoken_bytes[0]])])
+                continue
+
+            bytes_pairs = []
+            for i in range(len(pretoken_bytes) - 1):
+                bytes_pairs.append((bytes([pretoken_bytes[i]]), bytes([pretoken_bytes[i+1]])))
             
             bytes_2_ranks = dict()
             for i in range(len(self.merges)):
-                if self.merges[i] in bytes_list:
-                    bytes_2_ranks[self.merges] = i
+                if self.merges[i] in bytes_pairs:
+                    bytes_2_ranks[self.merges[i]] = i
             
-            while True:
+            while len(bytes_2_ranks) > 0:
                 # 找到rank最小的byte_pair在list中的index
-                idx, bytes_pair = self.index_bytes_pair(bytes_2_ranks, bytes_list)
-                bytes_list[idx] = bytes_list[idx] + bytes_list[idx+1]
-                del bytes_list[idx+1]
-                del bytes_2_ranks[bytes_pair]
+                best_match = min(bytes_2_ranks, key = lambda x:x[1])
+                lowest_pair = best_match
+                new_bytes = best_match[0] + best_match[1]
+                idx_in_pairs = bytes_pairs.index(lowest_pair)
 
-                if idx < len(bytes_list):
-                    if (bytes_list[idx], bytes_list[idx + 1]) in self.merges:
-                        new_pair = (bytes_list[idx], bytes_list[idx + 1])
-                        bytes_2_ranks[new_pair] = self.merges.index(new_pair)
-                if idx > 1:
-                    if (bytes_list[idx - 1], bytes_list[idx]) in self.merges:
-                        new_pair = (bytes_list[idx - 1], bytes_list[idx])
-                        bytes_2_ranks[new_pair] = self.merges.index(new_pair)
-                
-                if len(bytes_2_ranks) == 0:
-                    break
+                if idx_in_pairs > 0:
+                    pair = (bytes_pairs[idx_in_pairs - 1][0], new_bytes)
+                    if pair in self.merges:
+                        bytes_2_ranks[pair] = self.merges.index(pair)
+                    if bytes_pairs[idx_in_pairs - 1] in self.merges:
+                        del bytes_2_ranks[bytes_pairs[idx_in_pairs - 1]]
+                    bytes_pairs[idx_in_pairs - 1] = pair
+                    
+                if idx_in_pairs < len(bytes_pairs) - 1:
+                    pair = (new_bytes, bytes_pairs[idx_in_pairs + 1][1])
+                    if pair in self.merges:
+                        bytes_2_ranks[pair] = self.merges.index(pair)
+                    if bytes_pairs[idx_in_pairs + 1] in self.merges:
+                        del bytes_2_ranks[bytes_pairs[idx_in_pairs + 1]]
+                    bytes_pairs[idx_in_pairs + 1] = pair
+                del bytes_pairs[idx_in_pairs]
+                del bytes_2_ranks[lowest_pair]
+
+            bytes_list = [first[0] for first in bytes_pairs]
+            bytes_list.append(bytes_pairs[-1][1])
+            
+            print(bytes_list)
             # 得到每个bytes对应的ID,然后加到encoded_list里
             num_list = []
             for bts in bytes_list:
                 num_list.append(self.vocab_inverse[bts])
+            
             encoded_list.extend(num_list)
         
         return encoded_list
+    
+
+if __name__ == '__main__':
+    vocab_path = 'vocab.pkl'
+    merges_path = 'merges.pkl'
+    tokenizer = Tokenizer.from_files(vocab_path, merges_path, special_tokens=["<s>", "</s>"])
+    text = "<s>hello world, bitch.</s>"
+    print("ready to encode: ", text)
+    print("encoded text is: ", tokenizer.encode(text))
